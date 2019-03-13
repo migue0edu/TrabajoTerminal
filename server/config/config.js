173e18dfac0552aa67248a8fcb63108a54bb3b31
Miguel E. Rodríguez <migue0mpx@gmail.com>
2019-03-13 01:15:00 -0600

    Estructurado dentro de proyecto no Node

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,4 @@
+/*Puerto del servidor*/
 process.env.PORT = process.env.PORT || 3000;
+/*Conexion con la base de datos local*/
+let urlDB = 'mongodb://localhost:27017/Protocolo';
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 require('./config/config');
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.use( require('./routes/index') );
 
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
+
 app.listen(process.env.PORT, () =>{
   console.log('Escuchano puerto: ', process.env.PORT);
-})
+});
